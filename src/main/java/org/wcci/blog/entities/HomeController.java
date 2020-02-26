f27d191c591a3253c5eb298798f36241fd87713f
daly richy <dalyeu@yahoo.fr>
2020-02-26 00:11:51 -0500

    Created home controller and fixed the view resolver

---
+++ src/main/java/org/wcci/blog/entities/HomeController.java
@@ -0,0 +1,22 @@
+package org.wcci.blog.entities;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/index")
+    public String home(){
+
+        return "index";
+    }
+
+
+
+
+}
